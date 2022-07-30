2b237bb449f548a16d421110faefbc5d3469366a
Tijil Chhabra <tijilchhabra@gmail.com>
2022-07-30 23:54:24 +0530

    css n templates

--- Tool/static/js/app.js
+++ Tool/static/js/app.js
@@ -1,17 +1,54 @@
-//loader
-$(window).on("load", ()=> {
-    $(".loader-container").fadeOut(1000);
+
+$(window, document, undefined).ready(function () {
+
+    $('input').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+    $('textarea').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+});
+
+
+
+
+
+$(document).ready(function ($) {
+
+    $(".uploadlogo").change(function () {
+        var filename = readURL(this);
+        $(this).parent().children('span').html(filename);
+    });
+
+    function readURL(input) {
+        var url = input.value;
+        var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+        if (input.files && input.files[0] && (
+            ext == "csv"
+        )) {
+            var path = $(input).val();
+            var filename = path.replace(/^.*\\/, "");
+            $('.fileUpload span').html('Uploaded Proof : ' + filename);
+            return "Selected file : " + filename;
+        } else {
+            $(input).val("");
+            return "Only csv formats are allowed!";
+        }
+    }
+
+});
+
+$('#pills-tab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
 })
 
-//responsive navbar
-$(".toggleNav").click(() => {
-    $(".toggleNav").toggleClass("close");
-    $(".navbar-responsive").toggleClass("open");
-    $(".navbar-responsive__link").toggleClass("open__link");
-})
-
-//make navbar visible
-document.addEventListener("scroll", () => {
-    const $navbar = $(".navbar");
-    $navbar.toggleClass('sticky', $(this).scrollTop() > 250);
-})
+console.log('Xino <3 Ncrypt')
